06eb31d14b4dc08a13c685ec7e663324d3d39dda
Nathan Roett <Nroett@lcs.on.ca>
2026-02-09 14:44:18 -0500

    Initial Commit

---
+++ Block Layout/Block_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Block_LayoutApp.swift
+//  Block Layout
+//
+//  Created by Nathan Roett on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Block_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
